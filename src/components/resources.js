1f95354c14d27de1881987406943e29401ac862f
Nicholas Kneafsey <nkneafsey@natera.com>
2016-11-05 23:37:40 -0700

    Higher order component example

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Recipe
+      <ul>
+        <li>1 Cup Sugar</li>
+        <li>2 Cups Water</li>
+        <li>3 Cups Flour</li>
+      </ul>
+    </div>
+  )
+}
